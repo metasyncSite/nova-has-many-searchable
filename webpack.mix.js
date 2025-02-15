105b17d9cd391d960d4fe39d8d568e1883eea9b7
Metasync.site <157399850+metasyncSite@users.noreply.github.com>
2025-02-15 21:48:36 +0200

    Update webpack.mix.js with possible for debug from vendor

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,14 @@
-let mix = require('laravel-mix')
-let tailwindcss = require("tailwindcss")
+const mix = require('laravel-mix')
+const tailwindcss = require("tailwindcss")
+const path = require('path')
 
 require('./nova.mix')
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
-  .vue({ version: 3 })
-  .postCss("resources/css/field.css", "css", [tailwindcss("tailwind.config.js")])
-  .nova('acme/has-many-searchable')
+    .setPublicPath('dist')
+    .js('resources/js/field.js', 'js')
+    .vue({version: 3})
+    .postCss("resources/css/field.css", "css", [tailwindcss("tailwind.config.js")])
+    .alias({
+        'laravel-nova': path.join(__dirname, '../../laravel/nova/resources/js/mixins/packages.js'),
+    })
